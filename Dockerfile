3455f6f4f8ee7ffe9ca3db4c2cc34a23011fa194
shanemacbride <smacbride@mail.csuchico.edu>
2017-10-09 16:58:34 -0700

    LOK-17 Need to fix plugin installations for matrix auth to work

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,11 @@ FROM jenkins/jenkins:2.83
 
 MAINTAINER "lok@liatrio.com"
 
-ENV ADMIN_PASSWORD="admin123"
-ENV JAVA_OPTS="-Djenkins.install.runSetupWizard=false"
+ENV ADMIN_USERNAME="admin" \
+    ADMIN_PASSWORD="admin123" \
+    JAVA_OPTS="-Djenkins.install.runSetupWizard=false"
 
-#COPY resources/plugins /usr/share/jenkins/ref/plugins
+#COPY resources/plugins.txt /usr/share/jenkins/ref/plugins
 #COPY resources/config.xml /usr/share/jenkins/ref/config.xml
 #COPY resources/jobs /usr/share/jenkins/ref/jobs/
 COPY resources/init.groovy.d/ /usr/share/jenkins/ref/init.groovy.d/
--- resources/init.groovy.d/security.groovy
+++ resources/init.groovy.d/security.groovy
@@ -1,19 +1,65 @@
 #!groovy
- 
+
+import hudson.model.*
 import jenkins.model.*
 import hudson.security.*
 import jenkins.security.s2m.AdminWhitelistRule
 
-def env = System.getenv()
+Jenkins.instance.updateCenter.getPlugin("matrix-auth:1.7").deploy()
+
 def instance = Jenkins.getInstance()
+
+def env = System.getenv()
+def username = env['ADMIN_USERNAME']
+def password = env['ADMIN_PASSWORD']
  
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("admin", env['ADMIN_PASSWORD'])
+hudsonRealm.createAccount(username, password)
 instance.setSecurityRealm(hudsonRealm)
  
-def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-instance.setAuthorizationStrategy(strategy)
+def matrixStrategy = new GlobalMatrixAuthorizationStrategy()
+
+// Jenkins User
+matrixStrategy.add(hudson.model.Item.BUILD, 'jenkins')
+
+// Anonoymous Permissions
+matrixStrategy.add(Jenkins.READ, 'anonymous')
+matrixStrategy.add(hudson.model.Item.READ, 'anonymous')
+matrixStrategy.add(hudson.model.View.READ, 'anonymous')
+
+// General Permissions
+matrixStrategy.add(Jenkins.ADMINISTER, username)
+matrixStrategy.add(Jenkins.RUN_SCRIPTS, username)
+matrixStrategy.add(Jenkins.READ, username)
+
+// Job Permissions
+matrixStrategy.add(hudson.model.Item.BUILD, username)
+matrixStrategy.add(hudson.model.Item.CANCEL, username)
+matrixStrategy.add(hudson.model.Item.CONFIGURE, username)
+matrixStrategy.add(hudson.model.Item.CREATE, username)
+matrixStrategy.add(hudson.model.Item.DELETE, username)
+matrixStrategy.add(hudson.model.Item.DISCOVER, username)
+matrixStrategy.add(hudson.model.Item.EXTENDED_READ, username)
+matrixStrategy.add(hudson.model.Item.READ, username)
+matrixStrategy.add(hudson.model.Item.WIPEOUT, username)
+matrixStrategy.add(hudson.model.Item.WORKSPACE, username)
+
+// View Permissions
+matrixStrategy.add(hudson.model.View.CONFIGURE, username)
+matrixStrategy.add(hudson.model.View.CREATE, username)
+matrixStrategy.add(hudson.model.View.DELETE, username)
+matrixStrategy.add(hudson.model.View.READ, username)
+
+// Run Permissions
+matrixStrategy.add(hudson.model.Run.DELETE, username)
+matrixStrategy.add(hudson.model.Run.UPDATE, username)
+matrixStrategy.add(hudson.model.Run.ARTIFACTS, username)
+
+// Misc. Permissions
+matrixStrategy.add(hudson.scm.SCM.TAG, username)
+matrixStrategy.add(hudson.PluginManager.UPLOAD_PLUGINS, username)
+matrixStrategy.add(hudson.PlguinManager.CONFIGURE_UPDATECENTER, username)
+
+instance.setAuthorizationStrategy(matrixStrategy)
 instance.save()
- 
-Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
 
