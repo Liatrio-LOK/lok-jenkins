739e2a292b5b7477c76457fe5d4e6e8c2e52cce4
Matthew Dias <matthewpdias@gmail.com>
2017-10-11 15:53:08 -0700

    Lok 17 jenkins security (#3)
    * LOK-17 Created basic security setup
    * LOK-17 Converted Admin password to Env Var
    * updated groovy security script filenames to conform to new underscore naming strategy
    * created configMap references in the jenkins deployment yaml to run security initialization with username and password sourced as environment variables
    * LOK-17 changed configMap names to better reflect their usage

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,4 @@ FROM jenkins/jenkins:2.83
 
 MAINTAINER "lok@liatrio.com"
 
-#COPY resources/plugins /usr/share/jenkins/ref/plugins
-#COPY resources/config.xml /usr/share/jenkins/ref/config.xml
-#COPY resources/jobs /usr/share/jenkins/ref/jobs/
+ENV JAVA_OPTS="-Djenkins.install.runSetupWizard=false"
---
+++ resources/init.groovy.d/security.groovy
@@ -0,0 +1,23 @@
+#!groovy
+
+import hudson.model.*
+import jenkins.model.*
+import hudson.security.*
+import jenkins.security.s2m.AdminWhitelistRule
+import com.cloudbees.plugins.credentials.*
+
+def instance = Jenkins.getInstance()
+
+def env = System.getenv()
+def username = env['ADMIN_USERNAME']
+def password = env['ADMIN_PASSWORD']
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount(username, password)
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new GlobalMatrixAuthorizationStrategy()
+strategy.add(Jenkins.ADMINISTER, username)
+instance.setAuthorizationStrategy(strategy)
+
+instance.save()
